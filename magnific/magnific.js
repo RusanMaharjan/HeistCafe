4cc07952848d0c5aec918d9f3b447210451d64c1
RusanMaharjan <rusanmhz098@gmail.com>
2021-03-03 21:50:06 +0545

    first commit

---
+++ magnific/magnific.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#gallery-center').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image'
+        // other options
+
+       gallery: {
+            // options for gallery
+            enabled: true
+          }
+      });
+  });
